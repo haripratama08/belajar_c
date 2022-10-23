64c97e315424bd262db9660b756cb1490c79f5d5
haripratama08 <47474661+haripratama08@users.noreply.github.com>
2022-10-23 15:28:07 +0700

    materi operator aritmatika

---
+++ aritmatikaOperator.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    // +, -, *, /, %, ^,  decrement,  increment
+    // double a = 10;
+    // double b = 12;
+
+    // double hasil;
+
+    // hasil = a + b;
+    // printf("Hasil a + b = %.2lf \n", hasil);
+
+    // hasil = a - b;
+    // printf("Hasil a - b = %.2lf ", hasil);
+
+    // hasil = a * b;
+    // printf("Hasil a * b = %.2lf \n", hasil);
+
+    // hasil = a / b;
+    // printf("Hasil a / b = %.2lf \n", hasil);
+
+    // int c = 5;
+    // int d = 2;
+    // int hasilmod;
+    // hasilmod = c % d;
+    // printf("Hasil a mod b = %d \n", hasilmod);
+
+    return 0;
+}
