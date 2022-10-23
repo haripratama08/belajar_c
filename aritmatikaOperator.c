f453f697d3799254071809401f5fd31e726e1326
haripratama08 <47474661+haripratama08@users.noreply.github.com>
2022-10-23 16:42:37 +0700

    materi aritmatika baru

--- aritmatikaOperator.c
+++ aritmatikaOperator.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
-#include <math.h>
+#include <math.h> // kalau pangkat harus menggunakan library ini
 
 int main()
 {
     // +, -, *, /, %, ^,  decrement,  increment
-    // double a = 10;
-    // double b = 12;
+    double a = 10;
+    double b = 12;
 
-    // double hasil;
+    double hasil;
+    
+    hasil = a++;
+    // hasil =  a ,  a = a + 1
+    
+    // hasil = ++a;
+    // a = a + 1 , hasil = a 
+    
+    printf("Hasil a++ = %.2lf \n",  a);
+    
+    // hasil = pow(a, b);
+    // printf("Hasil 10 pangkat 12 = %.2lf \n",  hasil);
 
     // hasil = a + b;
-    // printf("Hasil a + b = %.2lf \n", hasil);
+    // printf("Hasil a + b = %.3f \n", hasil);
 
     // hasil = a - b;
     // printf("Hasil a - b = %.2lf ", hasil);
@@ -26,6 +37,6 @@ int main()
     // int hasilmod;
     // hasilmod = c % d;
     // printf("Hasil a mod b = %d \n", hasilmod);
-
+    
     return 0;
 }
--- inputOutput.c
+++ inputOutput.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
 
 // tipe data 
+    
 // int  : 1, 2, 3, 4, 5, 6, 7, 8, 9, 10        ||   %d
 // long : int tetapi memiliki memory yang lebih banyak || %l     
+
 // double : 3.14, 2.5, 3.19                    ||   %lf    
 // float  : sama seperti double tetapi lebih kecil memory nya     ||  %f 
+    
+    
 // String ===== Char (untuk satu huruf) dan Char[20] (untuk lebih dari satu huruf)    || %c     || %s
-// boolean : true dan false (1 atau 0)  ||
+// boolean : true dan false (1 atau 0)  || 
     
 void main(){
 // SCAN 
+    
 // Memasukan nama
-char nama[15];
+char nama[100];
+// Melakuan scan terhadap nama 
 scanf("%[^\n]",  &nama); getchar();
 // scanf("%s", &nama); getchar();
-
 // scanf("%s",  &nama); getchar();
 
-
-// Memasukan gender
+// Melakukan scan terhadao gender 
 char gender;
 scanf("%c",  &gender); getchar();
 
-// Nilai
 double nilai;
+// Melakukan scan terhadap input nilai 
 scanf("%lf",  &nilai); getchar();
 
-printf("Nama : %s\n",  nama);
 
+
+// Melakukan print nama terhadap inputan yang diberikan
+printf("Nama : %s\n",  nama);
+// Melakukan print gender terhadap inputan yang diberikan
 printf("Gender : %c\n",  gender);
-printf("Nilai : %.3lf\n",  nilai);
+// Melakukan print nilai terhadap inputan yang diberikan
+printf("Nilai : %.2f\n",  nilai);
+
+// printf("Nilai : %.3lf\n", nilai") ==========> format untuk membuat 3 angka dibelakang koma
 }
