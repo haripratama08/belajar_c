c2095405eb69a8e48d4cd6d48d85695b8d85eff9
Hari Agung Pratama <pratamahari236@gmail.com>
2022-11-13 16:01:09 +0700

    Materi array tambahan

--- array.c
+++ array.c
@@ -2,11 +2,56 @@
 
 int main()
 {
+
     // ARRAY 1 DIMENSI
-    //  int x[10]; // artinya adalah variable x ada 10
+    int c[3]; // artinya adalah variable x ada 10
+    c[0] = 1;
+    c[1] = 2;
+    c[2] = 1;
+
     //  x[2] = 5; // artinya isi dari array no 2 adalah 5
     //  printf("%d\n", x[2]); // print nilai dari x[2]
-    //  printf("%d\n", x[0]); 
+    //  printf("%d\n", x[0]);
+    // ARRAY 2 DIMENSI
+    int x[2][2] = {
+        {1, 2},
+        {1, 2},
+    };
+
+    int z[5][5] = {
+        {1, 2, 3, 4, 5},
+        {1, 2, 3, 4, 5},
+        {1, 2, 3, 4, 5},
+        {1, 2, 3, 4, 5},
+        {1, 2, 3, 4, 5},
+    };
+    // printf("%d", sqrt(sizeof(x)/sizeof(x[0][0])));
+
+    printf("%s", "Masukan panjang array X");
+    int lengthArrayX;
+    scanf("%d", &lengthArrayX);
+    getchar();
+    printf("%s", "Masukan panjang array Y");
+    int lengthArrayY;
+    scanf("%d", &lengthArrayY);
+    getchar();
+
+    for (int i = 0; i < lengthArrayX; i++)
+    {
+        for (int y = 0; y < lengthArrayY; y++)
+        {
+            printf("%d ", z[i][y]);
+        }
+        printf("\n");
+    }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int y = 0; y < 5; y++)
+    //     {
+    //         printf("%d", z[i][y]);
+    //     }
+    //     printf("%c\n");
+    // }
 
     // ARRAY 2 DIMENSI
     //  int y[10][30]; // array ke basamping 10 ke bawah 30
@@ -21,12 +66,21 @@ int main()
     // 1 2 3 4 5
     // 1 2 3 4 5
 
-    for (int i = 1; i < 3; i++)
-    {
-        for (int y = 1; y < 3; y++)
-        {
-            printf("%d\n", i);
-            printf("%d", y);
-        }
-    }
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     for (int y = 0; y < 2; y++)
+    //     {
+    //         // printf("%d", i);
+    //         // printf("%s", ",");
+    //         // printf("%d\n", y);
+    //         // x[i][y] = 10;
+    //         printf("%d", x[i][y]);
+    //         printf("%c", y == 3 - 1);
+    //         // printf("%s", y == 2 - 1);
+
+    //         // printf("%c", " \n"[y == 2 - 1]);
+    //     }
+    //     printf("%c\n");
+    //     // printf("%d\n", x);
+    // }
 }
--- looping.c
+++ looping.c
@@ -4,13 +4,21 @@ int main()
 {
 
     // LOOPING
-    //  terdiri dari 3 komponen utama yaitu for (deklarasi;  operasi ;komputasi){
+    //  terdiri dari 3 komponen utama yaitu for (deklarasi;  operasi/limitasi/batasan ;komputasi){
     //  }
 
     // METHOD 1 FOR
 
-    // for(int i = 0; i < 5; i++){
-    //     printf("%d",i);
+    // for (int i = 0;  // #1
+    //      i < 5;  // #3
+    //      i++)  // #2
+    // {
+    //     printf("%d", i);
+    // }
+
+    // for (int i = 7; i > 0; i = i - 2)
+    // {
+    //     printf("%d\n", i);
     // }
 
     // 7
@@ -19,23 +27,40 @@ int main()
     // 1
 
     // METHOD 2 DO-WHILE
+
     // int i = 0;
     // do
     // {
     //     printf("%d", i);
-    //     i++;
+    //     // i++;
+    //     i = i + 1;
     // } while (i < 5);
 
     // LOOPING 2 DIMENSI
 
-    // for (int i = 0; i < 5; i++)
-    // {
-    //     printf("%d/n", i);
-    //     for (int y = 0; y < 5; y++)
-    //     {
-    //         printf("%d", y);
-    //     }
-    // }
-
+    for (int i = 0; i < 4; i++)
+    {
+        for (int y = 0; y < 4; y++)
+        {
+            printf("%d", i*i);
+        }
+    }
     return 0;
+    // 0000111144449999
+    
+    //11111
+    //22222
+    //33333
+    //44444
+    //55555
+    
+    
+    //12345
+    //12345
+    //12345
+    //12345
+    //12345
+
+    
+    
 }
